1976b0809db2e7ff2c87575fe429bda4467b09fd
SethMaxson <maxsonseth@gmail.com>
2023-03-23 17:19:05 -0500

    vite base fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/arcade-mayhem/",
+  base: "https://sethmaxson.github.io/arcade-mayhem/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
